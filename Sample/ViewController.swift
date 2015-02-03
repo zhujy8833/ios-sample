7dfd23da5c27ae785b6455c9cbbb76d4a35e71a2
Jiangyue Zhu <zhujy8833@gmail.com>
2015-02-02 19:09:39 -0800

    Initial Commit

---
+++ Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Jiangyue Zhu on 2/2/15.
+//  Copyright (c) 2015 Jiangyue Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
